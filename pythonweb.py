34bf18e251d041934997d0bac4c7e65b2032e791
Kayla Braun <go1018bx@go.minneapolis.edu>
2017-10-04 15:59:21 -0500

    twitter

--- pythonweb.py
+++ pythonweb.py
@@ -2,7 +2,7 @@ from flickrapi import FlickrAPI
 import wikipedia
 from flask import Flask, render_template, request
 import os
-
+import tweepy
 app = Flask(__name__)
 
 
@@ -13,6 +13,7 @@ def index():
     wikiText = None
     flickrImage = None
     flickrImage2 = None
+    twitterText = None
 
     if request.method == 'POST' and 'searchText' in request.form:
         searchText = request.form['searchText']
@@ -33,8 +34,24 @@ def index():
 
         flickrImage2 = results['photos']['photo'][1]['url_l']
 
+
+
+
+
+        ACCESS_KEY =
+        ACCESS_SECRET =
+        CONSUMER_KEY =
+        CONSUMER_SECRET =
+        auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+        api = tweepy.API(auth)
+        bannas = api.search(searchText)
+        twitterText = ""
+        for b in bannas:
+           twitterText += b.text
+
     return render_template('index.html', flickrImage=flickrImage, flickrImage2=flickrImage2, wikiText=wikiText,
-                           wikiImage=wikiImage, searchText=searchText)
+                           wikiImage=wikiImage, searchText=searchText, twitterText=twitterText)
 
 
 if __name__ == '__main__':
