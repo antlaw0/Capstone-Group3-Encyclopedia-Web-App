bf022c1ed51f9cce8d15388e664257613bc8d51b
AAA <aaa@aaa.com>
2017-10-05 17:16:44 -0500

    fixed twitter api key issue

--- Results.py
+++ Results.py
@@ -17,10 +17,11 @@ def get_result(api_raw, search_text):
     api = get_api(api_raw.lower())
     if api == 0:
         # Twitter
-        ACCESS_KEY = ''
-        ACCESS_SECRET = ''
-        CONSUMER_KEY = ''
-        CONSUMER_SECRET = ''
+        ACCESS_KEY = os.environ['ACCESS_KEY']
+        ACCESS_SECRET = os.environ['ACCESS_SECRET']
+        CONSUMER_KEY = os.environ['CONSUMER_KEY']
+        CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+        
         auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
         auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
         api = tweepy.API(auth)
