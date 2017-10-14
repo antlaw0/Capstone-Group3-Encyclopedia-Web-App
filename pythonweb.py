d03273040c99b37c986e4af78e9156e93a7bd0f1
AAA <aaa@aaa.com>
2017-10-14 14:54:06 -0500

    Implemented input validation on registration page

--- pythonweb.py
+++ pythonweb.py
@@ -40,20 +40,39 @@ def index():
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
 
-    message = None
+    messages = []
+    
     if request.method=='POST':
         username = request.form['username']
         password = request.form['password']
+		
+        #check username length
+        if len(username) < 8:
+            messages.append("Invallid username- usernames must be at least 8 characters in length.")
+        if len(username) > 20:
+            messages.append("Invallid username- The entered username is too long. Usernames must be 20 characters or less in length.")
+        
+        #check password length
+        if len(password) < 8:
+            messages.append("Invallid password- passwords must be at least 8 characters long.")
+        if len(password) > 20:
+            messages.append("Invallid password- The password you entered is too long. Passwords must not be longer than 20 characters in length.")
+        
+        #if no error messages
+        if len(messages) != 0:
+            #return registration page with new error message(s)
+            return render_template('registration.html', messages=messages)
+        
         if dbHandler.userExists(username) == True:
-            message="UserAlready exists"
-            return render_template('registration.html', message=message)
+            messages.append("UserAlready exists")
+            return render_template('registration.html', messages=messages)
         else:
             #insert new user
             dbHandler.insertUser(username, password)
             session['username'] = username
             return redirect(url_for('user'))
     else:
-        return render_template('registration.html', message=message)
+        return render_template('registration.html', messages=messages)
     
 	
 @app.route('/results', methods=['GET', 'POST'])
